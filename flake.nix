a7f5338f225dc256957358f104e857f16bfe19c0
Daniel Guest <daniel.david.guest@gmail.com>
2025-07-18 17:51:20 +0100

    initialised as flake managed cargo project

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Manage resource bookings";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs; mkShell {
+          packages = [ cargo rustc rustfmt rustPackages.clippy rust-analyzer ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      }
+    );
+}
